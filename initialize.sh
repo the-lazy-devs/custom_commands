89602bc315e6a3b0e60aca7bf918fbbbaf6b4c45
jamesspargo <jspargo@thoughtworks.com>
2016-10-15 18:09:07 -0400

    JS - wrap the functionality in function calls to make the code more expressive

--- initialize.sh
+++ initialize.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-if [[ -d "$HOME/.bin" ]]; then
-    echo "$HOME/.bin already exists. nothing to do"
-else
-    echo "creating $HOME/.bin"
-    mkdir $HOME/.bin
-fi
-
-DOTFILES=(.bash_profile .bashrc)
-for FILE in ${DOTFILES[@]}; do
-    if grep -q '$HOME/.bin' "$HOME/$FILE" ; then
-        echo "$HOME/.bin is added to the PATH variable in $FILE already. nothing to do". 
+function create_bin_directory () {
+    if [[ -d "$HOME/.bin" ]]; then
+        echo "$HOME/.bin already exists. nothing to do"
     else
-        echo "updating ~/$FILE to add $HOME/.bin to the PATH variable"
-        echo 'export PATH="$PATH:$HOME/.bin"' >> "$HOME/$FILE"
+        echo "creating $HOME/.bin"
+        mkdir $HOME/.bin
     fi
-done
+}
 
-SCRIPTS=(gis gl gd)
-for SCRIPT in ${SCRIPTS[@]}; do
-    if [[  -L "$HOME/.bin/${SCRIPT}" ]]; then
-        echo "symlink for ${SCRIPT} is already present. nothing to do."
-    else
-        echo "creating a symlink for ${SCRIPT}"
-        ln -s "$HOME/.custom_git/${SCRIPT}.sh" "$HOME/.bin/${SCRIPT}"
-    fi
-done
+function update_path_variable () {
+    DOTFILES=(.bash_profile .bashrc)
+    for FILE in ${DOTFILES[@]}; do
+        if grep -q '$HOME/.bin' "$HOME/$FILE" ; then
+            echo "$HOME/.bin is added to the PATH variable in $FILE already. nothing to do". 
+        else
+            echo "updating ~/$FILE to add $HOME/.bin to the PATH variable"
+            echo 'export PATH="$PATH:$HOME/.bin"' >> "$HOME/$FILE"
+        fi
+    done
+}
+
+function create_symlinks () {
+    SCRIPTS=(gis gl gd)
+    for SCRIPT in ${SCRIPTS[@]}; do
+        if [[  -L "$HOME/.bin/${SCRIPT}" ]]; then
+            echo "symlink for ${SCRIPT} is already present. nothing to do."
+        else
+            echo "creating a symlink for ${SCRIPT}"
+            ln -s "$HOME/.custom_git/${SCRIPT}.sh" "$HOME/.bin/${SCRIPT}"
+        fi
+    done
+}
+
+create_bin_directory
+update_path_variable
+create_symlinks
