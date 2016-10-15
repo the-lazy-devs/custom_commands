24882d9bc6ee1abafcb99ad2d6bddf38c1b832cc
jamesspargo <jspargo@thoughtworks.com>
2016-10-15 02:36:36 -0400

    JS - add symlinking to the run_me_first script; add the gis script

--- run_me_first.sh
+++ run_me_first.sh
@@ -13,12 +13,21 @@ else
 fi
 
 DOTFILES=(.bash_profile .bashrc)
-
 for FILE in ${DOTFILES[@]}; do
     if grep -q '$HOME/.bin' "$HOME/$FILE" ; then
         echo "$HOME/.bin is added to the PATH variable in $FILE already. nothing to do". 
     else
         echo "updating ~/$FILE to add $HOME/.bin to the PATH variable"
         echo 'export PATH="$PATH:$HOME/.bin"' >> "$HOME/$FILE"
+    fi
+done
+
+SCRIPTS=(gis)
+for SCRIPT in ${SCRIPTS[@]}; do
+    if [[  -L "$HOME/.bin/${SCRIPT}" ]]; then
+        echo "symlink for ${SCRIPT} is already present. nothing to do."
+    else
+        echo "creating a symlink for ${SCRIPT}"
+        ln -s "$HOME/.custom_git/${SCRIPT}.sh" "$HOME/.bin/${SCRIPT}"
     fi
 done
