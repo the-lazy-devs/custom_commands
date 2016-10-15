41f58fb9f52f98a49eed90551e492e821c72dcba
jamesspargo <jspargo@thoughtworks.com>
2016-10-14 23:18:12 -0400

    JS - add swap files to gitignore; update .bash_profile instead of .bashrc

--- run_me_first.sh
+++ run_me_first.sh
@@ -11,10 +11,9 @@ else
     mkdir $HOME/.bin
 fi
 
-#if [[ ":$PATH:" == *"$HOME/.bin"* ]]; then
-if grep -q '$HOME/.bin' "$HOME/.bashrc" ; then
-    echo "$HOME/.bin is added to the PATH variable in .bashrc already. nothing to do". 
+if grep -q '$HOME/.bin' "$HOME/.bash_profile" ; then
+    echo "$HOME/.bin is added to the PATH variable in .bash_profile already. nothing to do". 
 else
-    echo "updating ~/.bashrc to add $HOME/.bin to the PATH variable"
-    echo 'export $PATH="$PATH:$HOME/.bin' >> "$HOME/.bashrc"
+    echo "updating ~/.bash_profile to add $HOME/.bin to the PATH variable"
+    echo 'export PATH="$PATH:$HOME/.bin"' >> "$HOME/.bash_profile"
 fi
