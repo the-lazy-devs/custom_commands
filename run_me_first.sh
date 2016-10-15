615414f62e758f55d15b8f2074028c6fa7af62db
jamesspargo <jspargo@thoughtworks.com>
2016-10-14 23:37:45 -0400

    JS - add the ~/.bin directory to run_me_first.sh

--- run_me_first.sh
+++ run_me_first.sh
@@ -17,3 +17,10 @@ else
     echo "updating ~/.bash_profile to add $HOME/.bin to the PATH variable"
     echo 'export PATH="$PATH:$HOME/.bin"' >> "$HOME/.bash_profile"
 fi
+
+if grep -q '$HOME/.bin' "$HOME/.bashrc" ; then
+    echo "$HOME/.bin is added to the PATH variable in .bashrc already. nothing to do". 
+else
+    echo "updating ~/.bashrc to add $HOME/.bin to the PATH variable"
+    echo 'export PATH="$PATH:$HOME/.bin"' >> "$HOME/.bashrc"
+fi
