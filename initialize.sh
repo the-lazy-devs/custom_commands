e8104a4b90776ee97e9acc9c6543c3063be96287
Karun Japhet <karun@japhet.in>
2016-10-26 11:29:40 +0530

    KJ - make project path a variable

--- initialize.sh
+++ initialize.sh
@@ -13,7 +13,7 @@ function update_path_variable () {
     DOTFILES=(.bash_profile .bashrc)
     for FILE in ${DOTFILES[@]}; do
         if grep -q '$HOME/.bin' "$HOME/$FILE" ; then
-            echo "$HOME/.bin is added to the PATH variable in $FILE already. nothing to do". 
+            echo "$HOME/.bin is added to the PATH variable in $FILE already. nothing to do".
         else
             echo "updating ~/$FILE to add $HOME/.bin to the PATH variable"
             echo 'export PATH="$PATH:$HOME/.bin"' >> "$HOME/$FILE"
@@ -22,23 +22,26 @@ function update_path_variable () {
 }
 
 function create_symlinks () {
+    SCRIPT_DIR_LOCATION=$1
     SCRIPTS=(gis gl gd)
     for SCRIPT in ${SCRIPTS[@]}; do
         if [[  -L "$HOME/.bin/${SCRIPT}" ]]; then
             echo "symlink for ${SCRIPT} is already present. nothing to do."
         else
             echo "creating a symlink for ${SCRIPT}"
-            ln -s "$HOME/.custom_commands/${SCRIPT}.sh" "$HOME/.bin/${SCRIPT}"
+            ln -s "$SCRIPT_DIR_LOCATION/${SCRIPT}.sh" "$HOME/.bin/${SCRIPT}"
         fi
     done
 }
 
 function update_alias_sourcing () {
+    SCRIPT_DIR_LOCATION=$1
     echo 'adding the aliases to .bash_profile'
-    echo 'source $HOME/.custom_commands/alias_admin_commands.sh'  >> "$HOME/.bash_profile"
+    echo "source $SCRIPT_DIR_LOCATION/alias_admin_commands.sh" >> "$HOME/.bash_profile"
 }
 
+SCRIPT_DIR_LOCATION=$HOME/.custom_commands
 create_bin_directory
 update_path_variable
-create_symlinks
-update_alias_sourcing
+create_symlinks $SCRIPT_DIR_LOCATION
+update_alias_sourcing $SCRIPT_DIR_LOCATION
