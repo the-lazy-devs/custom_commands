994550ad28b12c5e62039626a600ea3c2ef5a17b
Karun Japhet <karun@japhet.in>
2016-10-28 06:02:36 +0530

    KJ - improve color formatting of messages

--- functions.sh
+++ functions.sh
@@ -2,12 +2,12 @@
 
 function create_bin_directory() {
   if [[ -d "$HOME/.bin" ]]; then
-    print "${PYELLOW}${PBOLD}[No action]${PRESET}${PYELLOW} $HOME/.bin already exists${PRESET}"
+    print "[${PBOLD}No action$PRESET] $PBLUE$HOME/.bin ${PYELLOW}already exists$PRESET"
   else
     if [ "$DRY_RUN" = true ]; then
-      print "${PYELLOW}Would create $HOME/.bin${PRESET}"
+      print "${PYELLOW}Would create $PBLUE$HOME/.bin$PRESET"
     else
-      print "${PGREEN}Creating $HOME/.bin${PRESET}"
+      print "${PGREEN}Creating $PBLUE$HOME/.bin$PRESET"
       mkdir $HOME/.bin
     fi
   fi
@@ -19,12 +19,12 @@ function update_path_variable() {
   local DOT_FILES=("${@}")
   for DOT_FILE in ${DOT_FILES[@]}; do
     if grep -sq '$HOME/.bin' "$DOT_FILE"; then
-      print "${PYELLOW}${PBOLD}[No action]${PRESET}${PYELLOW} $HOME/.bin is added to the PATH variable in ${DOT_FILE##*/} already${PRESET}"
+      print "[${PBOLD}No action$PRESET] $PBLUE$HOME/.bin ${PYELLOW}already exists in PATH in $PBLUE${DOT_FILE##*/}$PRESET"
     else
       if [ "$DRY_RUN" = true ]; then
-        print "${PYELLOW}Would update $DOT_FILE to add $HOME/.bin to the PATH variable${PRESET}"
+        print "${PYELLOW}Would update $PBLUE$DOT_FILE$PYELLOW to add $PBLUE$HOME/.bin$PYELLOW to the PATH variable$PRESET"
       else
-        print "${PGREEN}Updating $DOT_FILE to add $HOME/.bin to the PATH variable${PRESET}"
+        print "${PGREEN}Updating ${PBLUE}$DOT_FILE${PGREEN} to add ${PBLUE}$HOME/.bin${PBLUE} to the PATH variable${PRESET}"
         echo 'export PATH="$PATH:$HOME/.bin"' >>"$DOT_FILE"
       fi
     fi
@@ -42,12 +42,12 @@ function create_symlinks() {
     local SCRIPT=${SCRIPT_FILE_NAME%.*}
 
     if [[ -L "$HOME/.bin/${SCRIPT}" ]]; then
-      print "${PYELLOW}${PBOLD}[No action]${PRESET}${PYELLOW} symlink for ${SCRIPT} is already present${PRESET}"
+      print "[${PBOLD}No action$PRESET]$PYELLOW symlink for $PBLUE$SCRIPT$PYELLOW is already present$PRESET"
     else
       if [ "$DRY_RUN" = true ]; then
-        print "${PYELLOW}Would create a symlink for $SCRIPT${PRESET}"
+        print "${PYELLOW}Would create a symlink for $PBLUE$SCRIPT$PRESET"
       else
-        print "${PGREEN}Creating a symlink for $SCRIPT${PRESET}"
+        print "${PGREEN}Creating a symlink for $PBLUE$SCRIPT$PRESET"
         ln -s "$SCRIPT_DIR_LOCATION/commands/$SCRIPT_FILE_NAME" "$HOME/.bin/$SCRIPT"
       fi
     fi
@@ -66,12 +66,12 @@ function update_alias_sourcing() {
     local PROFILE_FILE_NAME=${PROFILE_FILE_LOCATION##*/}
 
     if grep -sq "source $SCRIPT" "$PROFILE_FILE_LOCATION"; then
-      print "${PYELLOW}${PBOLD}[No action]${PRESET}${PYELLOW} $SCRIPT_NAME has already been sourced in $PROFILE_FILE_NAME${PRESET}"
+      print "[${PBOLD}No action$PRESET] $PBLUE$SCRIPT_NAME$PYELLOW has already been sourced in $PBLUE$PROFILE_FILE_NAME$PRESET"
     else
       if [ "$DRY_RUN" = true ]; then
-        print "${PYELLOW}Would add the aliases from $SCRIPT_NAME to $PROFILE_FILE_NAME${PRESET}"
+        print "${PYELLOW}Would add the aliases from $PBLUE$SCRIPT_NAME$PYELLOW to $PBLUE$PROFILE_FILE_NAME$PRESET"
       else
-        print "${PGREEN}Adding the aliases from $SCRIPT_NAME to $PROFILE_FILE_NAME${PRESET}"
+        print "${PGREEN}Adding the aliases from $PBLUE$SCRIPT_NAME$PGREEN to $PBLUE$PROFILE_FILE_NAME$PRESET"
         echo "source $SCRIPT" >> $PROFILE_FILE_LOCATION
       fi
     fi
