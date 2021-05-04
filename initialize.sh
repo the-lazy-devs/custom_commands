0ee7f66aabe64e74a6ebde75652e3046b667b960
spargonaut <spargonaut@gmail.com>
2021-05-04 10:53:09 -0700

    fix spelling in DEPRECATED message

--- initialize.sh
+++ initialize.sh
@@ -3,6 +3,6 @@
 printf "\n\n"
 printf "*****  DEPRECATED  *****\n"
 printf "Sorry, this command has beenn Deprecates\n"
-printf "use ./create_custom_caommands.sh instead\n"
+printf "use ./create_custom_commands.sh instead\n"
 printf "*****  DEPRECATED  *****\n"
 printf "\n\n"
