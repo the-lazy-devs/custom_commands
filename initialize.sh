c99e971da7bcfe491bd5939d0345659320526c96
spargonaut <spargonaut@gmail.com>
2021-05-04 21:58:57 -0700

    delete the deprecated initialize script

--- initialize.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-printf "\n\n"
-printf "*****  DEPRECATED  *****\n"
-printf "Sorry, this command has beenn Deprecates\n"
-printf "use ./create_custom_commands.sh instead\n"
-printf "*****  DEPRECATED  *****\n"
-printf "\n\n"
